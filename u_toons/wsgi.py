ad3f8d08e0e52f4c67e6ae4ba0d67f1c57bf4346
alexquar <alexanderquarrie@gmail.com>
2024-04-14 14:47:41 -0400

    init

---
+++ u_toons/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for u_toons project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'u_toons.settings')
+
+application = get_wsgi_application()
